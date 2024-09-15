82257ca8c7de2b744afb66b5f72c29b982278153
Maria Nascimento <marianascimentomn14@gmail.com>
2024-09-15 11:21:05 -0300

    correção da tela de submissão de materiais

---
+++ javaScript.js
@@ -0,0 +1,8 @@
+
+// Botão de buscar arquivos.
+const buscarBtn = document.querySelector('.buscar-btn');
+const meuArquivo = document.getElementById('meuArquivo');
+
+buscarBtn.addEventListener('click', () => {
+    meuArquivo.click();
+});
