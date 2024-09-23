207748b979be67ca15466a64d559cb841dff2932
liviaandressa <liviaandressa61@gmail.com>
2024-09-23 19:29:13 -0300

    ajusta vizualização de arquivos

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,19 @@
+/** @format */
 
 // Botão de buscar arquivos.
 const buscarBtn = document.querySelector('.buscar-btn');
 const meuArquivo = document.getElementById('meuArquivo');
 
 buscarBtn.addEventListener('click', () => {
-    meuArquivo.click();
+  meuArquivo.click();
+});
+
+document.getElementById('confirmSave').addEventListener('click', function () {
+  // Aqui você pode colocar a ação de salvar os dados
+  // Fechar o modal após salvar
+  var modal = bootstrap.Modal.getInstance(
+    document.getElementById('confirmModal')
+  );
+  modal.hide();
+  alert('Dados salvos com sucesso!');
 });
