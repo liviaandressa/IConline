c3a2c26bd4feefe47795a97abe7e65f3541595e2
liviaandressa <liviaandressa61@gmail.com>
2024-09-23 22:33:36 -0300

    ajusta botão de baixar documento

--- javaScript.js
+++ javaScript.js
@@ -16,4 +16,18 @@ document.getElementById('confirmSave').addEventListener('click', function () {
   );
   modal.hide();
   alert('Dados salvos com sucesso!');
+  location.reload();
 });
+document.getElementById('downloadBtn').onclick = function () {
+  // Criar um link para o arquivo
+  const link = document.createElement('a');
+  link.href = 'Documentos/IConline/Prova-AB1.txt'; // Substitua pelo caminho real do seu arquivo
+  link.download = 'Prova-AB1.txt'; // Nome do arquivo a ser baixado
+
+  // Adicionar o link ao documento e simular o clique
+  document.body.appendChild(link);
+  link.click();
+
+  // Remover o link do documento
+  document.body.removeChild(link);
+};
